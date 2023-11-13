a971b6cfedd4e76df8b364a893f30e61108af204
Daniël van Eeden <git@myname.nl>
2023-11-13 08:02:09 +0100

    Support numbers and underscore in variable names

--- src/main.go
+++ src/main.go
@@ -364,7 +364,7 @@ func (t *tester) Run() error {
 		case Q_ERROR:
 			t.expectedErrs = strings.Split(strings.TrimSpace(s), ",")
 		case Q_ECHO:
-			varSearch := regexp.MustCompile("\\$([A-Za-z]+)( |$)")
+			varSearch := regexp.MustCompile("\\$([A-Za-z0-9_]+)( |$)")
 			s := varSearch.ReplaceAllStringFunc(s, func(s string) string {
 				return os.Getenv(varSearch.FindStringSubmatch(s)[1])
 			})
